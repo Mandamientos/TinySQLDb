df55b437e7dc4beffc4b3cbca50ce458203b696f
cluis11 <luiscm_94@hotmail.com>
2024-09-27 21:01:22 -0600

    Se crea el archivo de la tabla en la carpera de base de datos

--- StoreSystem/CatalogOperations/AddTable.cs
+++ StoreSystem/CatalogOperations/AddTable.cs
@@ -8,11 +8,12 @@ namespace StoreSystem.CatalogOperations
 {
     public class AddTable
     {
-        private const string tabDBPath = @"C:\TinySQLDb\SystemCatalog\SystemTables.dat";
+        private const string tabDBPath = @"C:\TinySQLDb\";
 
         public static void execute(string dbName, string tabName)
         {
-            using (FileStream stream = new FileStream(tabDBPath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            string path = tabDBPath + @"SystemCatalog\SystemTables.dat";
+            using (FileStream stream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite))
             {
                 if (stream.Length == 0)
                 {
@@ -23,18 +24,36 @@ namespace StoreSystem.CatalogOperations
                 }
             }
 
-            using (FileStream stream = File.Open(tabDBPath, FileMode.Append, FileAccess.Write))
+            using (FileStream stream = File.Open(path, FileMode.Append, FileAccess.Write))
             using (BinaryWriter writer = new(stream))
             {
                 string result = $"{dbName},{tabName}\n";
                 writer.Write(result);
             }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(dbName);
+            Console.WriteLine();
+            Console.WriteLine();
+            path = tabDBPath + $@"\Databases\{dbName}\{tabName}.table";
+            try
+            {
+                using (FileStream stream = File.Open(path, FileMode.CreateNew, FileAccess.Write))
+                {
+                    // Empty file is created
+                }
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("File already exists.");
+            }
         }
 
         public static bool checkExistence(string dbName, string tabName)
         {
+            string path = tabDBPath + @"SystemCatalog\SystemTables.dat";
             string result = $"{dbName},{tabName}";
-            using (BinaryReader reader = new BinaryReader(File.Open(tabDBPath, FileMode.Open)))
+            using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
             {
                 while (reader.BaseStream.Position != reader.BaseStream.Length)
                 {
